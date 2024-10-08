d41ff09c567509cd3586904d27b2186689b60c73
mou@inokscapital.com <MOU@inokscapital.com>
2024-10-08 14:05:27 +0200

    in progress

--- entities.py
+++ entities.py
@@ -4,54 +4,67 @@ class Etat(Enum):
     VIVANT = 1
     MORT = 0
 
-class Cellule: 
+class Cellule:
     def __init__(self, x, y, etat):
         self.x = x
         self.y = y
         self.etat = etat
-        
-    #methodes 
-    def getEtat(self):
-        return self.etat
+        self.etat_futur = etat  # Ajout d'un attribut pour l'état futur
+
+    @property
+    def etat(self):
+        return self._etat
     
-    def setEtat(self, etat):
-        self.etat = etat
+    @etat.setter
+    def etat(self, valeur):
+        self._etat = valeur
+
+    # Méthode pour définir l'état futur
+    def definir_etat_futur(self, etat_futur):
+        self.etat_futur = etat_futur
+    
+    def appliquer_etat_futur(self):
+        self.etat = self.etat_futur
+    
+    def basculer_etat(self):
+        self.etat = 1 if self.etat == 0 else 0
+
 
 class Tableau:
     def __init__(self, largeur, hauteur):
         self.largeur = largeur
         self.hauteur = hauteur
-        self.grille = [[0 for _ in range(largeur)] for _ in range(hauteur)]  # 0 pour morte, 1 pour vivante
+        self.grille = [[0 for _ in range(largeur)] for _ in range(hauteur)] 
 
-    def initialiser(self, cellules_initiales):
-        for x, y in cellules_initiales:
-            self.grille[y][x] = 1  # 1 représente une cellule vivante
+    def initialiser(self, cellules_vivantes):
+        for x, y in cellules_vivantes:
+            self.grille[y][x] = 1 
 
     def afficher(self):
         for ligne in self.grille:
             print(" ".join(str(cellule) for cellule in ligne))
 
-    def compter_voisins(self, x, y):
-        voisins = 0
+    def compter_voisins_vivants(self, x, y):
+        voisins_vivants = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if (i == 0 and j == 0) or not (0 <= x + i < self.largeur) or not (0 <= y + j < self.hauteur):
                     continue
-                voisins += self.grille[y + j][x + i]
-        return voisins
+                voisins_vivants += self.grille[y + j][x + i]
+        return voisins_vivants
 
-    def mise_a_jour(self):
+    def mettre_a_jour_grille(self):
         nouvelle_grille = [[0 for _ in range(self.largeur)] for _ in range(self.hauteur)]
         for y in range(self.hauteur):
             for x in range(self.largeur):
-                voisins = self.compter_voisins(x, y)
+                voisins_vivants = self.compter_voisins_vivants(x, y)
                 if self.grille[y][x] == 1:  # Cellule vivante
-                    if voisins in [2, 3]:
+                    if voisins_vivants in [2, 3]:
                         nouvelle_grille[y][x] = 1  # Reste vivante
                 else:  # Cellule morte
-                    if voisins == 3:
+                    if voisins_vivants == 3:
                         nouvelle_grille[y][x] = 1  # Devient vivante
         self.grille = nouvelle_grille
 
-    def reinitialiser(self):
+    def reinitialiser_grille(self):
         self.grille = [[0 for _ in range(self.largeur)] for _ in range(self.hauteur)]
--- jeux-de-la-vie.py
+++ jeux-de-la-vie.py
@@ -5,5 +5,46 @@ import pygame
 import entities
 
 Cellule = entities.Cellule
+Tableau = entities.Tableau
+EtatEnum = entities.Etat
 
 
+
+largeut_grille = 20
+hauteur_grille = 20
+taille_cellule = 20
+
+def initialiser_grille(largeut_grille, hauteur_grille):
+    cellules_vivantes = [(1, 1), (2, 2), (2, 3), (3, 1), (3, 2)]
+    tableau = Tableau(largeut_grille, hauteur_grille)
+    tableau.initialiser(cellules_vivantes)
+    return tableau
+
+def afficher_grille(tableau, screen, taille_cellule):
+    screen.fill((255, 255, 255))
+    for y in range(tableau.hauteur):
+        for x in range(tableau.largeur):
+            cellule = tableau.grille[y][x]
+            couleur = (255, 255, 255) if cellule.etat == EtatEnum.MORT else (0, 0, 0)
+            pygame.draw.rect(screen, couleur, (x * taille_cellule, y * taille_cellule, taille_cellule, taille_cellule))
+    pygame.display.flip()
+
+def mettre_a_jour_grille(tableau):
+    for ligne in tableau.grille: 
+        for cellule in ligne:
+            nb_voisins_vivants = tableau.compter_voisins_vivants(cellule.x, cellule.y)
+            if(cellule.etat == EtatEnum.VIVANT):
+                if nb_voisins_vivants < 2 or nb_voisins_vivants > 3:
+                    cellule.definir_etat_futur(EtatEnum.MORT)
+            else:
+                if(nb_voisins_vivants == 3):
+                    cellule.definir_etat_futur(EtatEnum.VIVANT)
+
+
+if(__name__ == "__main__"):
+    grille = initialiser_grille(largeut_grille, hauteur_grille)
+
+    for _ in range(10):
+        mettre_a_jour_grille(grille)
+        afficher_grille(grille, taille_cellule)
+        pygame.time.wait(1000)
